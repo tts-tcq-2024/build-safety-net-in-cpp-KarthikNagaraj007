950a478a1e8fbdc124627867df9cbaa266fb1700
KarthikNagaraj007 <82349176+KarthikNagaraj007@users.noreply.github.com>
2024-07-01 20:34:10 +0530

    Update_Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -2,43 +2,47 @@
 #include <cctype>
 #include <unordered_map>
 
-// Function to initialize the Soundex code map
-std::unordered_map<char, char> initializeSoundexMap() {
-    return {
-        {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
-        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
-        {'D', '3'}, {'T', '3'},
-        {'L', '4'},
-        {'M', '5'}, {'N', '5'},
-        {'R', '6'}
-    };
+const std::unordered_map<char, char> soundexTable = {
+    {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+    {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, 
+    {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
+    {'D', '3'}, {'T', '3'},
+    {'L', '4'},
+    {'M', '5'}, {'N', '5'},
+    {'R', '6'}
+};
+
+char getSoundexCode(char c) {
+    c = toupper(c);
+    auto it = soundexTable.find(c);
+    return (it != soundexTable.end()) ? it->second : '0';
 }
 
-char getSoundexCode(char c, const std::unordered_map<char, char>& soundexMap) {
-    c = toupper(c);
-    auto it = soundexMap.find(c);
-    return it != soundexMap.end() ? it->second : '0'; // For A, E, I, O, U, H, W, Y
+std::string getInitialSoundex(const std::string& name) {
+    if (name.empty()) return "";
+    return std::string(1, toupper(name[0]));
+}
+
+std::string padSoundex(std::string soundex) {
+    while (soundex.length() < 4) {
+        soundex += '0';
+    }
+    return soundex;
 }
 
 std::string generateSoundex(const std::string& name) {
-    if (name.empty()) return "";
+    std::string soundex = getInitialSoundex(name);
+    if (soundex.empty()) return soundex;
 
-    static const auto soundexMap = initializeSoundexMap();
-    std::string soundex(1, toupper(name[0]));
-    char prevCode = getSoundexCode(name[0], soundexMap);
+    char prevCode = getSoundexCode(soundex[0]);
 
     for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
-        char code = getSoundexCode(name[i], soundexMap);
+        char code = getSoundexCode(name[i]);
         if (code != '0' && code != prevCode) {
             soundex += code;
             prevCode = code;
         }
     }
 
-    while (soundex.length() < 4) {
-        soundex += '0';
-    }
-
-    return soundex;
+    return padSoundex(soundex);
 }
-
