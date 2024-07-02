7cd04960f89122fc6bda23b9a139eb697124f576
KarthikNagaraj007 <82349176+KarthikNagaraj007@users.noreply.github.com>
2024-07-02 18:40:19 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,7 +4,5 @@
 #include <string>
 
 std::string generateSoundex(const std::string& name);
-std::string padSoundex(const std::string& soundex);
-std::string buildInitialSoundex(const std::string& name);
 
 #endif // SOUNDEX_H
